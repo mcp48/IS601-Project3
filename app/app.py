e766d402b047408b9e6f0a92220e65b9b3827c16
mcp48 <michael.c.payne83@gmail.com>
2021-07-21 21:05:32 -0400

    Feature: Amended app.py file for index template

--- app/app.py
+++ app/app.py
@@ -28,7 +28,15 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Michael'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
